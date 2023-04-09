b466da2c08f6959a84aa4fb584120da40d947789
Tetjana0508 <dancingtatyana@gmail.com>
2023-04-09 19:06:33 +0300

    Д/з 1, частина 1 (неактивні кнопки)

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -10,23 +10,32 @@ const startBtn = document.querySelector("[data-start]");
 const stopBtn = document.querySelector("[data-stop]");
 let timerId = null;
 let isActive = false;
+stopBtn.disabled = true;
+
 
 startBtn.addEventListener("click", () => { /* При клике на кнопку «Start» запустим интервал и будем каждую секунду выводить в консоль строку. Используем Math.random() чтобы строки были разные */
+  // startBtn.disabled = false;
   if (isActive) {
     return;
   }
   isActive = true;
+  // startBtn.disabled = false;
   timerId = setInterval(() => { /* Метод setInterval() - способ повторения кода, интервал запускает выполнение функции не один раз, а регулярно повторяет её через указанный промежуток времени. */
     // isActive = true;
     console.log(`getRandomHexColor()`);
     backgroundColorEl.style.backgroundColor = getRandomHexColor(); /* стилизируем body функцией getRandomHexColor */
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
   }, 1000);
 });
 // console.log(startBtn);
 stopBtn.addEventListener("click", () => { /* По клику на кнопку «Stop» вызовем clearInterval() и передадим идентификатор интервала который надо остановить. */
+  
   if (isActive = false) {
     return;
   }
   clearInterval(timerId);
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
   console.log(`Interval with id ${timerId} has stopped!`);
 });
